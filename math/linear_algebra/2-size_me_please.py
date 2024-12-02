8227f7bcfff4cfc7c0f1715ea1c7747a7d0f1502
Rdrg974 <hassany.rodrigue@gmail.com>
2024-12-02 14:08:26 +0100

    Fix pycodestyle + documentation

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
+"""Function that calculates the shape of a matrix"""
+
+
 def matrix_shape(matrix):
-    """Recursive function to get the shape of a matrix"""
+    """
+    Recursive function to get the shape of a matrix
+    Parameters:
+        matrix (list): A list (or nested lists) representing the matrix.
+    Returns:
+        list: A list of integers representing the shape of the matrix.
+    """
     if type(matrix) is not list:
         return []
     return [len(matrix)] + matrix_shape(matrix[0])
