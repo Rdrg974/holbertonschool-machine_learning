0ffb3287c04cd0e92b128822860d361e9e1f0c5d
Rdrg974 <hassany.rodrigue@gmail.com>
2024-12-02 14:04:33 +0100

    Documentez ma fonction

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 def matrix_shape(matrix):
+    """Recursive function to get the shape of a matrix"""
     if type(matrix) is not list:
         return []
     return [len(matrix)] + matrix_shape(matrix[0])
