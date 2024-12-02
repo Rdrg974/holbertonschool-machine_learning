3a50cb0ca7d2fbb64a4362905a7eb7285dee4af5
Rdrg974 <hassany.rodrigue@gmail.com>
2024-12-02 15:20:03 +0100

    Howdy Partner

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""A function that concatenates two arrays"""
+
+
+def cat_arrays(arr1, arr2):
+    """
+    Concatenate two arrays
+    Parameters:
+        arr1 (list): a list of integers/floats
+        arr2 (list): a list of integers/floats
+    Returns:
+        list: a new list of integers/floats
+    """
+    return arr1 + arr2
