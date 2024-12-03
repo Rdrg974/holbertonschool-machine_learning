279f9308683fa5e36ad949d14146f1923c8af2e4
Rdrg974 <hassany.rodrigue@gmail.com>
2024-12-03 09:24:07 +0100

    The Western Exchange

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""A function that that transposes matrix"""
+
+
+def np_transpose(matrix):
+    """
+    Transposes a matrix
+    Parameters:
+        - matrix (numpy.ndarray): a matrix to transpose
+    Returns:
+        numpy.ndarray: the transposed matrix
+    """
+    return matrix.transpose()
