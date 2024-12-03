711b2a8c7bb4a4938c3a57eeb9ba4a268b0d6bf6
Rdrg974 <hassany.rodrigue@gmail.com>
2024-12-03 09:20:20 +0100

    I’ll Use My Scale

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""A function that calculates the shape of a numpy.ndarray"""
+
+
+def np_shape(matrix):
+    """
+    Calculates the shape of a numpy.ndarray
+    Parameters:
+        matrix (numpy.ndarray): A numpy.ndarray
+    Returns:
+        tuple: A tuple containing the shape of the matrix
+    """
+    return matrix.shape
